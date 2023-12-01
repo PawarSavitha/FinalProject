47b63b9edb25be4712c96107503afdba7b05a5d8
Mahender Krishna Jatta <mahendra552@icloud.com>
2023-12-01 15:27:03 +0530

    User model modified

--- FinalProject_MoviesApp/Models/User.cs
+++ FinalProject_MoviesApp/Models/User.cs
@@ -5,7 +5,7 @@
         public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string City { get; set; }
+        public string City { get; set; } = string.Empty;
         public string Email { get; set; }
         public string Password { get; set; }
         public string Phone { get; set; } = string.Empty; //abcd
