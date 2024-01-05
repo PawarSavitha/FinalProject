a3e6bdf2272b52e9ab828e7ef9f02d0a880e1533
Pawar Savitha <114134446+PawarSavitha@users.noreply.github.com>
2024-01-05 14:43:05 +0530

    Update User.cs

--- FinalProject_MoviesApp/Models/User.cs
+++ FinalProject_MoviesApp/Models/User.cs
@@ -9,6 +9,6 @@
         public string Email { get; set; }
         public string Password { get; set; }
         public string Phone { get; set; } = string.Empty; 
-        public string Role { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty; // testing
     }
 }
